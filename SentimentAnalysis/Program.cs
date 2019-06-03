bd1e407d5e5c6e847d20b17d94135fbe00705d6a
Eduardo Júnior <ed.afram@gmail.com>
2019-06-03 01:42:46 -0300

    Add project files.

---
+++ SentimentAnalysis/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace SentimentAnalysis
+{
+    class Program
+    {
+        static readonly string _dataPath = 
+            Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            TrainTestData splitDataView = LoadData(mlContext);
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            Evaluate(mlContext, model, splitDataView.TestSet);
+            UseModelWithSingleItem(mlContext, model);
+        }
+
+        private static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        {
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = 
+                mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            SentimentData sampleStatement = new SentimentData
+            {
+                SentimentText = "very not good"
+            };
+            var resultprediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultprediction.SentimentText} | " +
+                $"Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Positive" : "Negative")} | " +
+                $"Probability: {resultprediction.Probability} ");
+
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+        }
+
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+
+        private static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainSet)
+        {
+            var estimator = mlContext.Transforms.Text
+                .FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(trainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            return model;
+        }
+
+        private static TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>
+                (_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+    }
+}
